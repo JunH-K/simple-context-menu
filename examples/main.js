f82936138251b8c3e1474eca4ade339734bdc967
kdeveloper87 <k.developer87@gmail.com>
2019-12-15 20:50:04 +0900

    [add] examples

---
+++ examples/main.js
@@ -0,0 +1,9 @@
+import ContextMenu from "./index";
+import { mock } from '../mock/mock';
+
+const contextMenu = new ContextMenu( document.querySelector( '#contextMenu' ) );
+
+contextMenu.register( '#target',
+  function (key) {
+    console.log( key )
+  }, mock );
